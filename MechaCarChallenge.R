0eaf76a1d5a8230b0f043885fc89bc87616c9626
spencerkopp <spencertkopp@gmail.com>
2022-10-03 13:55:11 -0400

    Finishing up deliverable 2, total summary and lot summary.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,3 +2,8 @@ library(tidyverse)
 mecha_csv <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 lm(mpg ~ AWD + ground_clearance + spoiler_angle + vehicle_weight + vehicle_length,data=mecha_csv) #generate multiple linear regression model
 summary(lm(mpg ~ AWD + ground_clearance + spoiler_angle + vehicle_weight + vehicle_length,data=mecha_csv))
+suspension <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- suspension %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), 
+                                          SD = sd(PSI), .groups = 'keep')
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI),Median = median(PSI), Variance = var(PSI),
+                                                                        SD = sd(PSI) , .groups = 'keep')
